88efe330a79f1e8324d0ac42f33deecf6bb463bf
makhambet <makha.98.29@mail.ru>
2019-06-19 12:06:10 +0600

    Add files via upload

---
+++ js/typeeed.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed : 40,
+    backSpeed : 80,
+    startDelay : 40, 
+    backSpeed : 40,
+    loop: true 
+  });
